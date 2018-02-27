dedad4eda40d8c23595a98e08ea9242e6468b620
huangshun <406846759@qq.com>
2018-02-27 16:23:05 +0800

    切换viewpager时背景变化

--- app/src/main/java/huangshun/it/com/androiddesignpattern/timeline/HomeTimeLineFragment.java
+++ app/src/main/java/huangshun/it/com/androiddesignpattern/timeline/HomeTimeLineFragment.java
@@ -8,6 +8,8 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,6 +27,7 @@ public class HomeTimeLineFragment extends Fragment {
     private View mStepView;//步数
     private View mSleepView;//睡眠
     private View mWeightView;//体重
+    private SummaryType mSummaryType = SummaryType.Step;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -59,5 +62,76 @@ public class HomeTimeLineFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        initListener();
+    }
+
+
+    /**
+     * 根据viewpager的滑动设置背景和字体颜色
+     */
+    private void setTypeBg(SummaryType summaryType) {
+        mBinding.timelineTop.tvTipStep.setBackground(null);
+        mBinding.timelineTop.tvTipSleep.setBackground(null);
+        mBinding.timelineTop.tvTipWeight.setBackground(null);
+        if (summaryType == SummaryType.Step) {
+            mBinding.timelineTop.tvTipStep.setTextColor(ContextCompat.getColor(getContext(), R.color.white));
+            mBinding.timelineTop.tvTipSleep.setTextColor(ContextCompat.getColor(getContext(), R.color.black));
+            mBinding.timelineTop.tvTipWeight.setTextColor(ContextCompat.getColor(getContext(), R.color.black));
+            mBinding.timelineTop.tvTipStep.setBackgroundResource(R.drawable.bg_timeline_shape);
+
+        } else if (summaryType == SummaryType.Sleep) {
+            mBinding.timelineTop.tvTipStep.setTextColor(ContextCompat.getColor(getContext(), R.color.black));
+            mBinding.timelineTop.tvTipSleep.setTextColor(ContextCompat.getColor(getContext(), R.color.white));
+            mBinding.timelineTop.tvTipWeight.setTextColor(ContextCompat.getColor(getContext(), R.color.black));
+            mBinding.timelineTop.tvTipSleep.setBackgroundResource(R.drawable.bg_timeline_shape);
+        } else {
+            mBinding.timelineTop.tvTipStep.setTextColor(ContextCompat.getColor(getContext(), R.color.black));
+            mBinding.timelineTop.tvTipSleep.setTextColor(ContextCompat.getColor(getContext(), R.color.black));
+            mBinding.timelineTop.tvTipWeight.setTextColor(ContextCompat.getColor(getContext(), R.color.white));
+            mBinding.timelineTop.tvTipWeight.setBackgroundResource(R.drawable.bg_timeline_shape);
+        }
+
+    }
+
+    private void initListener() {
+        mBinding.vpTopPreview.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                setTypeBg(SummaryType.valueOf(position));
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+
+    public enum SummaryType {
+        Step(0), Sleep(1), Weight(2);
+        private int typeInt;
+
+        SummaryType(int typeInt) {
+            this.typeInt = typeInt;
+        }
+
+        public int getTypeInt() {
+            return typeInt;
+        }
+
+        public static SummaryType valueOf(int i) {
+            if (Step.getTypeInt() == i) {
+                return SummaryType.Step;
+            } else if (Sleep.getTypeInt() == i) {
+                return SummaryType.Sleep;
+            } else {
+                return SummaryType.Weight;
+            }
+        }
     }
 }
